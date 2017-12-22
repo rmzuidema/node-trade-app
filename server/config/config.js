0c8aac417ccd4de625e571608977e0d4b2fc4c5d
rmzuidema <robert.m.zuidema@gmail.com>
2017-12-22 18:03:51 -0500

    Modified mongo connection string

--- server/config/config.js
+++ server/config/config.js
@@ -14,8 +14,8 @@ if (env === 'Local') {
  //       console.log('Process env ', process.env[key]);
     });
 }
-// if ( env === 'Local') {
-//     process.env.MONGOURI='mongodb://localhost:27017/NewTodoApp';
-// } else {
-//     process.env.MONGOURI='mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp';    
-// }
+if ( env === 'Local') {
+    process.env.MONGOURI='mongodb://localhost:27017/NewTodoApp';
+} else {
+    process.env.MONGOURI='mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp';    
+}
